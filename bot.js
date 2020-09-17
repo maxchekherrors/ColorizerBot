741db108e0ec65f0a99decb68327b1fecd8c6406
MaxChekh <max.chekh1313@gmail.com>
2020-09-17 17:08:00 +0300

    decompose server

--- bot.js
+++ bot.js
@@ -2,11 +2,15 @@
 const config = require('./config');
 const {Telegraf} = require('telegraf');
 const session = require('telegraf/session');
-const applayBotMiddleware = require('./botApi');
+const applyBotMiddleware = require('./botApi');
 const errorMiddleware = require('./middleware/error.midlleware');
+const spamMiddleware = require('./middleware/spam.midlleware');
+const updateMiddleware = require('./middleware/updateInfo.middleware');
 const bot = new Telegraf(`${config.bot.token}`);
-if(config.isTest||config.isDevelopment)bot.use(Telegraf.log());
-bot.use(session());
-bot.use(errorMiddleware);
-applayBotMiddleware(bot);
+if (config.isTest || config.isDevelopment) bot.use(Telegraf.log());
+bot.use(session())
+    .use(spamMiddleware)
+    .use(updateMiddleware)
+    .use(errorMiddleware);
+applyBotMiddleware(bot);
 module.exports = bot;
--- botApi/user/user.scene.js
+++ botApi/user/user.scene.js
@@ -1,11 +1,10 @@
 const Scene = require('telegraf/scenes/base');
 const controllers = require('./user.controller');
 const mainScene = new Scene('main');
-const spamMiddleware = require('../../middleware/spam.midlleware');
-const updateMiddleware = require('../../middleware/updateInfo.middleware');
+
 exports.mainScene = ()=>{
-	mainScene.start(updateMiddleware,controllers.start);
-	mainScene.on('photo',spamMiddleware,updateMiddleware,controllers.getPhoto);
+	mainScene.start(controllers.start);
+	mainScene.on('photo',controllers.getPhoto);
 	mainScene.on('message',controllers.error);
 	return mainScene;
 };
