90ae48839b0dd7887bee9b96fc02d0d54de20e44
MaxChekh <max.chekh1313@gmail.com>
2020-09-17 16:50:10 +0300

    fix mistakes

--- botApi/user/user.controller.js
+++ botApi/user/user.controller.js
@@ -5,7 +5,7 @@ exports.start = ({replyWithAudio,message})=>
 	replyWithAudio('CQACAgIAAxkBAAM0X14LhTzRbRyA2FtKF6aChQqS2TUAAoUJAAJrulhJ-8ST-hHeIJ4bBA', Extra
 		.HTML()
 		.load({
-			caption: `Welcome to the club ${message.from.first_name || 'body!'}\nSend me your <b>black and white</b> photo and I will try to <b>color</b> it)`
+			caption: `🤘🏾Welcome to the club ${message.from.first_name || 'body!'}\nSend me your <b>black and white</b> photo and I will try to <b>color</b> it)`
 		})
 	);
 
@@ -13,8 +13,8 @@ exports.getPhoto = async ctx=>{
 	const {photo} = ctx.message;
 	const {message:{from:{id:userId}}} = ctx;
 	const {file_id:fileId} = photo[photo.length-1];
-	await ctx.replyWithHTML('...');
-	await ctx.replyWithHTML( '<i>I need a little time for this.</i>>');
+	await ctx.replyWithHTML('👌🏿');
+	await ctx.replyWithHTML( '<i>I need a little time for this.</i>');
 	const file = await  ctx.telegram.getFileLink(fileId);
 	const colorizeUrl = await colorize(file);
 	await User.updateOne({_id:userId},{$push:{photos:fileId}});
@@ -23,4 +23,4 @@ exports.getPhoto = async ctx=>{
 		ctx.replyWithPhoto(colorizeUrl);
 };
 exports.error = ({replyWithHTML})=>
-	replyWithHTML('I don\'t give a fuck about your message. \n<b>Send me photo or get a fuck out of here.</b>');
+	replyWithHTML('I don\'t give a fuck about your message. \n<b>Send me photo or get a fuck out of here.</b>🤏');
